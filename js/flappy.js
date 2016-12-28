6a7be81c3c60f3632a368bd0e19af8936630cf20
zdutta <zdutta@purdue.edu>
2016-12-28 13:23:02 -0800

    Some more nueral functionality

--- js/flappy.js
+++ js/flappy.js
@@ -1,8 +1,97 @@
 //population or pool->individual genome->chromosome->gene 
 //fitness score assigned to each genome and breeding is done with the fittest
 //selection,crossover,mutation
+var gap = 250;
+var pipWidth = 127;
+
 var max = {
-	generations: 1,
-	organism: 1,
-	fitness: 0;
+	generation: 1,
+	genome: 1,
+	fitness: 0,
 }
+
+function coordinate(x,y){
+	this.x=x,
+	this.y=y		
+}
+
+function newChromosome(){
+	var chromosome = {};
+	chromosome.flapDistanceDX = Math.floor(Math.random()*440);
+	chromosome.flapDistanceDY = 0;//vertical distance from lower pipe
+	chromosome.fitness=0;
+	chromosome.realFitness=0;
+	chromosome.flapModifier=Math.random()*2;//the constant at which flap parameters are modified
+	chromosome.getFinalFlapDistance= function(speed){
+		return this.flapDistanceDX-(this.flapModifier*speed);
+	}
+	return chromosome;
+}
+
+var generation = 1;
+var chromosomes = [];
+var nextGen = [];
+
+const GENOME_PER_GEN = 10;
+
+function makeFirstGeneration(){
+	for(var i = 0; i<GENOME_PER_GEN;i++){
+		var chrom = newChromosome();
+		//chrom.flapDistanceDX = Math.floor(Math.random()*440);
+		//chrom.flapModifier = Math.random()*2;
+		chromosomes.push(chrom);
+	}
+}
+
+function neuralTick(piepDelay,dx,dy){//game speed and if there is upcoming obstable
+	if(d==undefined||d==null){//if no upcoming obstacles
+		return;
+	}
+	simulate(pipeDelay,dx,dy);
+}
+
+
+function simulate(pipeDelay,dx,dy){
+	if(isNearPipe())
+}
+
+function flap(){
+
+}
+
+function isNearPipe(){
+
+}
+
+function birdDied(fitness, realFitness){
+	if(realFitness>max.fitness){
+		max.fitness = realFitness;
+		max.generation = generation;
+		max.genome = count++;
+	}
+	chromosome()
+}
+
+function sortByDistance(){
+
+}
+
+function compareByDistance(){
+
+}
+
+function killHalf(){
+
+}
+
+function mate(){
+
+}
+
+function getGenAve(){
+
+}
+
+
+makeFirstGeneration();
+
