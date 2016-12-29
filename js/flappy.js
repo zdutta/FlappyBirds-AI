baa154bc868324c2c6e54e8512e0ce3bb067d927
zdutta <zdutta@purdue.edu>
2016-12-28 16:17:41 -0800

    Added to flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -22,8 +22,12 @@ function newChromosome(){
 	chromosome.fitness=0;
 	chromosome.realFitness=0;
 	chromosome.flapModifier=Math.random()*2;//the constant at which flap parameters are modified
-	chromosome.getFinalFlapDistance= function(speed){
+	//0-2 because flap can either be decreased with 0-1 or increased with 1-2 
+	chromosome.getFinalFlapDistanceDX= function(speed){//altering flap distance
 		return this.flapDistanceDX-(this.flapModifier*speed);
+	}
+	chromosome.getFinalFlapFistanceDY=function(speed){
+		return this.flapDistanceDY-(this.flapModifier*speed);
 	}
 	return chromosome;
 }
@@ -43,25 +47,41 @@ function makeFirstGeneration(){
 	}
 }
 
-function neuralTick(piepDelay,dx,dy){//game speed and if there is upcoming obstable
+function neuralTick(piepDelay,x,y){//game speed and if there is upcoming obstable
 	if(d==undefined||d==null){//if no upcoming obstacles
 		return;
 	}
-	simulate(pipeDelay,dx,dy);
+	simulate(pipeDelay,x,y);//(x,y) coordinate location of bird
 }
 
 
-function simulate(pipeDelay,dx,dy){
-	if(isNearPipe())
+function simulate(pipeDelay,x,y){
+	if(isNearPipe(x,Math.floor(chromosomes[count].getFinalFlapDistanceDX(pipeDelay)))){
+		while(y<Math.floor(chromosomes[count].getFinalFlapFistanceDY(pipeDelay))+gap){
+			flap();
+		}
+	}
+}
+
+function isNearPipe(birdx,pipex){
+	if(birdx>pipex-5){//&&birdx<pipex+20
+		true;
+	}else{
+		return false;
+	}
 }
 
 function flap(){
-
+	var event = new Event("jump");
+	event.keyCode = 32;//keycode for spacebar
+	event.witch = event.keycode;
+	event.altKey=false;
+	event.ctrlKey=false;//true
+	event.shiftKey=false;
+	event.metaKey=false;
+	document.dispatchEvent(event);
 }
 
-function isNearPipe(){
-
-}
 
 function birdDied(fitness, realFitness){
 	if(realFitness>max.fitness){
